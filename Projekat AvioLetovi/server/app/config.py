5e44548075b7c10510f57bcdc23b3bfd7bb5c756
Gleeshoux <148570953+Gleeshoux@users.noreply.github.com>
2026-01-15 16:04:54 +0100

    Dodat SQLEXPRESS u konfiguracioni string

--- Projekat AvioLetovi/server/app/config.py
+++ Projekat AvioLetovi/server/app/config.py
@@ -1,8 +1,7 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = (
-        "mssql+pyodbc://@localhost/dbAvioUsers"
-        "?driver=ODBC+Driver+17+for+SQL+Server"
-        "&trusted_connection=yes"
+         "mssql+pyodbc://@localhost\\SQLEXPRESS/dbAvioUsers"
+         "?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
