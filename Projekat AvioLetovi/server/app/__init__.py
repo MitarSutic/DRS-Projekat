1b97a314f7b63d37b78ecf06e99c8895830e8861
MitarSutic <mitar.razmena@gmail.com>
2026-02-08 11:43:59 +0100

    server init

--- Projekat AvioLetovi/server/app/__init__.py
+++ Projekat AvioLetovi/server/app/__init__.py
@@ -5,7 +5,7 @@ from .extensions import db, jwt, bcrypt, socketio, mail
 from .routes.auth import auth_bp
 from flask_mail import Mail, Message
 from app.routes.users import users_bp
-
+from .routes.flights_proxy import flights_bp
 
 def create_app():
     app = Flask(__name__)
@@ -18,6 +18,7 @@ def create_app():
     app.config.from_object(Config)
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(users_bp, url_prefix="/users")
+    app.register_blueprint(flights_bp, url_prefix="/api")
 
     CORS(app)
     db.init_app(app)
